c757472538c2d896b16f6b3580251ce6aead9127
Luis Filipe Guedes Mota <lmota@img.com.br>
2020-04-24 09:49:21 -0300

    Projeto Inicial

---
+++ DotNetCore_SignalR_Angular/Controllers/WeatherForecastController.cs
@@ -0,0 +1,46 @@
+﻿
+
+using System;
+using DotNetCore_SignalR_Angular.Hubs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Logging;
+
+namespace DotNetCore_SignalR_Angular.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        static HubConnection connection;
+
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+
+
+        [HttpGet]
+        public String Get()
+        {
+            if (connection == null)
+            {
+                connection = new Microsoft.AspNetCore.SignalR.Client.HubConnectionBuilder().WithUrl("https://localhost:44300/contador").Build();
+
+                connection.StartAsync();
+            }
+            connection.InvokeAsync("somaContador");
+
+            return "OK";
+        }
+    }
+}
---
+++ DotNetCore_SignalR_Angular/Hubs/ContadorHub.cs
@@ -0,0 +1,62 @@
+﻿
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCore_SignalR_Angular.Hubs
+{
+    public static class Contador
+    {
+        public static int valor;
+        public static IList<String> conectados = new List<string>();
+    }
+
+  
+
+
+    public class ContadorHub : Microsoft.AspNetCore.SignalR.Hub
+    {
+
+        public override Task OnConnectedAsync()
+        {
+
+            Contador.conectados.Add(Context.ConnectionId);
+
+            return base.OnConnectedAsync();
+        }
+
+        public async Task GetContador()
+        {
+            await Clients.Caller.SendAsync("getContador", Contador.valor);
+        }
+
+        public async Task SomaContador()
+        {
+            Contador.valor++;
+            await BroadCastContador();
+        }
+
+        public async Task BroadCastContador()
+        {
+            await Clients.All.SendAsync("broadcastContador", Contador.valor);
+        }
+
+        public Task OnConnected()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task OnReconnected()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task OnDisconnected(bool stopCalled)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
